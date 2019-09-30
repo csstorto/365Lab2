a6eca4a654859790af521a1b601acf2ba4701815
Cameron Storton <csstorto@calpoly.edu>
2019-09-30 12:54:15 -0700

    Reworked schoolsearch.py to function with the new data format

--- schoolsearch.py
+++ schoolsearch.py
@@ -6,15 +6,18 @@ GRADE = 2
 CLASSROOM = 3
 BUS = 4
 GPA = 5
-TLASTNAME = 6
-TFIRSTNAME = 7
+
+TLASTNAME = 0
+TFIRSTNAME = 1
+TCLASSROOM = 2
 
 NUM_GRADES = 7
 
 from collections import defaultdict
 
 def main():
-    students = get_students_as_array()
+    students = get_file_as_array("list.txt")
+    teachers = get_file_as_array("teachers.txt")
 
     inp = get_user_input()
     while inp not in ["Q", "QUIT"]:
@@ -28,11 +31,11 @@ def main():
         else:
             query_type = split_inp[0]
             query_args = split_inp[1].strip().split(" ")
-            parse_query(query_type, query_args, students)
+            parse_query(query_type, query_args, students, teachers)
 
         inp = get_user_input()
 
-def parse_query(query_type, query_args, students):
+def parse_query(query_type, query_args, students, teachers):
     query_type_dict={
         "S":s_query,
         "STUDENT":s_query,
@@ -46,17 +49,18 @@ def parse_query(query_type, query_args, students):
         "AVERAGE":a_query
     }
     parsing_function = query_type_dict.get(query_type, print_bad_query_msg)
-    parsing_function(query_args, students)
+    parsing_function(query_args, students, teachers)
 
-def s_query(query_args, students):
+def s_query(query_args, students, teachers):
     if len(query_args) is 1:
         last_name = query_args[0]
         for student in students:
             if student[0] == last_name:
+                teacher = get_teacher_by_classroom(teachers, student[CLASSROOM])
                 print("%s, %s, %s, %s, %s, %s"
                     % (student[STLASTNAME], student[STFIRSTNAME],
-                    student[GRADE], student[CLASSROOM], student[TLASTNAME],
-                    student[TFIRSTNAME]))
+                    student[GRADE], student[CLASSROOM], teacher[TLASTNAME],
+                    teacher[TFIRSTNAME]))
     elif len(query_args) is 2 and query_args[1] is ("B" or "BUS"):
         last_name = query_args[0]
         for student in students:
@@ -66,7 +70,7 @@ def s_query(query_args, students):
     else:
         print_bad_query_msg()
 
-def t_query(query_args, students):
+def t_query(query_args, students, teachers):
     if len(query_args) is not 1:
         print_bad_query_msg()
 
@@ -75,7 +79,7 @@ def t_query(query_args, students):
         if student[TLASTNAME] == last_name:
             print("%s, %s" % (student[STLASTNAME], student[STFIRSTNAME]))
 
-def b_query(query_args, students):
+def b_query(query_args, students, teachers):
     if len(query_args) is not 1:
         print_bad_query_msg()
     try:
@@ -89,7 +93,7 @@ def b_query(query_args, students):
         print("B[us] takes a valid integer, not %s" % (query_args[0]))
         print_bad_query_msg()
 
-def g_query(query_args, students):
+def g_query(query_args, students, teachers):
     try:
         if len(query_args) is 1:
             grade = int(query_args[0])
@@ -107,9 +111,10 @@ def g_query(query_args, students):
                             highest_gpa = float(student[GPA])
                             top_student = student
                 if top_student:
+                    teacher = get_teacher_by_classroom(teachers, top_student[CLASSROOM])
                     print("%s, %s, %.2f, %s, %s, %s"
                         % (top_student[STLASTNAME], top_student[STFIRSTNAME],
-                        highest_gpa, top_student[TLASTNAME], top_student[TFIRSTNAME],
+                        highest_gpa, teacher[TLASTNAME], teacher[TFIRSTNAME],
                         top_student[BUS]))
             elif query_args[1] is ("L" or "LOW"):
                 lowest_gpa = float(4.0)
@@ -120,10 +125,11 @@ def g_query(query_args, students):
                             lowest_gpa = float(student[GPA])
                             worst_student = student
                 if worst_student:
+                    teacher = get_teacher_by_classroom(teachers, worst_student[CLASSROOM])
                     print("%s, %s, %.2f, %s, %s, %s"
                         % (worst_student[STLASTNAME], worst_student[STFIRSTNAME],
-                        lowest_gpa, worst_student[TLASTNAME],
-                        worst_student[TFIRSTNAME], worst_student[BUS]))
+                        lowest_gpa, teacher[TLASTNAME],
+                        teacher[TFIRSTNAME], worst_student[BUS]))
             else:
                 print_bad_query_msg()
         else:
@@ -133,7 +139,7 @@ def g_query(query_args, students):
         print_bad_query_msg()
 
 
-def a_query(query_args, students):
+def a_query(query_args, students, teachers):
     if len(query_args) is not 1:
         print_bad_query_msg()
 
@@ -176,16 +182,24 @@ def print_bad_query_msg(*args):
 def get_user_input():
     return input("$ ").upper()
 
-def get_students_as_array():
+def get_teacher_by_classroom(teachers, classroom):
+    for teacher in teachers:
+        if teacher[TCLASSROOM] == classroom:
+            return teacher
+    print("Bad classroom value provided: " + classroom)
+
+def get_file_as_array(filename):
     try:
-        students_file = open("students.txt", "r")
-        students_array = []
-        for line in students_file.readlines():
-            students_array.append(line.strip().split(","))
-        return students_array
+        _file = open(filename, "r")
+        entry_array = []
+        for line in _file.readlines():
+            entry_array.append(line.strip().split(","))
+            for num in range(len(entry_array[-1])):
+                entry_array[-1][num] = entry_array[-1][num].strip()
+        return entry_array
     # Catch file not found
     except OSError as e:
-        print("students.txt not found in the current directory.")
+        print("''" + filename + " not found in the current directory.")
         exit(1)
 
 if __name__ == "__main__":
